2f74a380089d513e6edfab3e2bd51d040640077e
Gurjeet singh <gurjeet.sasan@gmail.com>
2016-01-25 23:16:02 +0530

    Array Programs

---
+++ array/simple/SimpleArray.java
@@ -0,0 +1,11 @@
+class SimpleArray{
+	public static void main(String args[] ){
+		//System.out.println( "Testing Arrays" );
+		int test[] = {1,2,3,4,5,6,7,8,9};
+		//System.out.println( test[4]);
+		for( int i: test ){
+			System.out.println("Number is: "+ i);
+		}
+		
+	}
+}
